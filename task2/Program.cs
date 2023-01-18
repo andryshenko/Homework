3b7d905e478e76a77453ae06e742cfc870df232e
olegandruschenko <andryshenko@gmail.com>
2023-01-18 08:43:22 +0300

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+System.Console.WriteLine("ВВедите первое число: ");
+int a;
+System.Console.WriteLine("ВВедите второе число: ");
+int a;
+
+if 
+    {
+        int a > b;
+        System.Console.ReadLine("Max") (a);
+    }
+    else
+    {
+        System.Console.ReadLine("Max") (b);
+    }
